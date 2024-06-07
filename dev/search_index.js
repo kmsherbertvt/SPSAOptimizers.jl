a2939c36e41479c4af49e2da2d399c793a9e1fc5
Documenter.jl <documenter@juliadocs.github.io>
2024-06-07 15:36:08 +0000

    build based on c2eb060

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SPSAOptimizers","category":"page"},{"location":"#SPSAOptimizers","page":"Home","title":"SPSAOptimizers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SPSAOptimizers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SPSAOptimizers]","category":"page"}]
+}
